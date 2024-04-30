4df8bc15561b6955332f54bfa92f27b1dfa899e2
xmarcjordanx <mhujanen1@gmail.com>
2024-04-29 22:34:53 -0600

    Update

--- server.js
+++ server.js
@@ -7,35 +7,21 @@ Description: Web API scaffolding for Movie API
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-//var authController = require('./auth');
+var authController = require('./auth');
 var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
 var Movie = require('./Movies');
-const mongoose = require('mongoose');
-// const {MongoClient}=require('mongodb');
-require('dotenv').config();
 
 var app = express();
-
-app.use(cors({
-    origin: 'https://csc3916-react-cd09f498c3d5.herokuapp.com/#/'
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-
 var router = express.Router();
-
-const uri = process.env.DB;
-const port = process.env.PORT || 8080;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 
 function getJSONObjectForMovieRequirement(req) {
     var json = {
@@ -59,11 +45,10 @@ router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
-        var user = new User({
-        name: req.body.name,
-        username: req.body.username,
-        password: req.body.password,
-        })
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
 
         user.save(function(err){
             if (err) {
@@ -87,6 +72,7 @@ router.post('/signin', function (req, res) {
         if (err) {
             res.send(err);
         }
+
         user.comparePassword(userNew.password, function(isMatch) {
             if (isMatch) {
                 var userToken = { id: user.id, username: user.username };
@@ -99,41 +85,109 @@ router.post('/signin', function (req, res) {
         })
     })
 });
-        //get /movies
-router.get('/movies', authJwtController.isAuthenticated, (req, res) =>{
-    Movie.find({title: { $exists: true}})
-        .then(movies => {
+
+router.route('/movies')
+.get((req, res) => {
+    Movie.find({
+        title: { $exists: true, $ne: null },
+        releaseDate: { $exists: true, $ne: null },
+        genre: { $exists: true, $ne: null },
+        actors: { $exists: true, $ne: null }
+    }, (err, movies) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
             res.status(200).json(movies);
-        })
-        .catch(error => {
-            console.error('Sorry, it looks like there was an error finding movies:', error);
-            res.status(500).json({error: 'An error has occurred while looking for this movies'});
-        });
-});
-        //post /movies
-router.post('/movies',authJwtController.isAuthenticated,(req, res) =>{
-    const {title, releaseDate, genre, actors} = req.body;
-    if (!title){
-        return res.status(400).json({error: 'Please entire a title of a new movie'});
+        }
+    });
+})
+
+.post((req, res) => {
+    const { title, releaseDate, genre, actors } = req.body;
+
+    if (!title || !releaseDate || !genre || !actors || actors.length === 0) {
+        return res.status(400).json({ error: 'Title, release date, genre, and at least one actor (actor name and character name) are required' });
     }
-    const newMovie = new Movie ({title, releaseDate, genre, actors});
 
-    newMovie.save()
-        .then(savedMovie =>{
-            res.status(200).json(savedMovie);
-        });
-});
-        
-
-router.delete('/movies', authJwtController.isAuthenticated, (req, res) => {
-        res.status(405).send({message: 'The Delete method is not yet supported.'});
+    try {
+        const movie = new Movie({ title, releaseDate, genre, actors });
+        movie.save();
+        res.status(200).json(movie);
+    } catch (error) {
+        console.error('Error creating movie:', error);
+        res.status(500).json({ error: 'Failed to create movie' });
+    }
 });
 
-router.all('/movies', authJwtController.isAuthenticated, (req, res) => {
-    res.status(405).send({message: 'This method is not supported.'});
+// router.route('/movies/:id')
+//     .delete((req, res) => {
+//         const movieId = req.params.id;
+
+//         // Find the movie by id and delete it
+//         Movie.findByIdAndDelete(movieId, (err, deletedMovie) => {
+//             if (err) {
+//                 console.error('Error deleting movie:', err);
+//                 return res.status(500).json({ error: 'Failed to delete movie' });
+//             }
+
+//             if (!deletedMovie) {
+//                 return res.status(404).json({ error: 'Movie not found' });
+//             }
+
+//             res.status(200).json({ message: 'Movie deleted successfully', deletedMovie });
+//         });
+//     });
+
+
+router.route('/movies/:title')
+.get((req, res) => {
+    const movieTitle = req.params.title;
+    Movie.find({ title: movieTitle }, (err, movie) => {
+        if (err) {
+            res.status(400).send(err);
+        } else if (movie.length === 0) {
+            res.status(404).json({ error: 'Movie not found' });
+        } else {
+            res.status(200).json(movie);
+        }
+    });
+})
+
+.put((req, res) => {
+    const currentTitle = req.params.title;
+    const newTitle = req.body.title;
+
+    // Find the movie by current title and update its title
+    Movie.findOneAndUpdate({ title: currentTitle }, { title: newTitle }, { new: true }, (err, updatedMovie) => {
+        if (err) {
+            res.status(400).send(err);
+        } else if (!updatedMovie) {
+            res.status(404).json({ error: 'Movie not found' });
+        } else {
+            res.status(200).json(updatedMovie);
+        }
+    });
+})
+
+
+
+.delete((req, res) => {
+    const movieTitle = req.params.title;
+
+    Movie.findOneAndDelete({ title: movieTitle }, (err, deletedMovie) => {
+        if (err) {
+            console.error('Error deleting movie:', err);
+            return res.status(500).json({ error: 'Failed to delete movie' });
+        }
+
+        if (!deletedMovie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        res.status(200).json({ message: 'Movie deleted successfully', deletedMovie });
+    });
 });
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
